2a3dec2ca90fe746fb8e84862c2bc02c8690266a
Sharpryan20 <sharpryan01@icloud.com>
2021-10-23 20:26:11 +0000

    Added rulesGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 const startButton = document.getElementById('start-btn')
+const rulesButton = document.getElementById('rules-btn')
 
 startButton.addEventListener('click', startGame)
+rulesButton.addEventListener('click', rulesGame)
 
 function startGame() {
     startButton.classList.add('hide')
 
+}
+
+function rulesGames() {
+    
 }
 
 function nextQuestion() {
