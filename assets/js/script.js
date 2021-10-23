0866b69f81ead4607f68898cc0392638bcafaed9
Sharpryan20 <sharpryan01@icloud.com>
2021-10-23 20:22:36 +0000

    Added event listener to startButton

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    startButton.classList.add('hide')
+
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+    
+}
