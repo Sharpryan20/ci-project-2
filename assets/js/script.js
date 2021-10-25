7732e5d083b3198c17bc6e5b567b77450ebef76c
Sharpryan20 <sharpryan01@icloud.com>
2021-10-25 22:20:56 +0000

    Added JS so when the rules button is clicked it will bring up a set of rules for the game and created functions to load up the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,64 @@
-const startButton = document.getElementById('start-btn')
-const rulesButton = document.getElementById('rules-btn')
-const ruleBox = document.getElementById('rules-container')
+const startButton = document.getElementById('start-btn');
+const questionContainer = document.getElementById('question-container')
+
+const rulesContainer  = document.getElementById('rules-container');
+const rulesButton = document.getElementById('rules-btn');
+const span = document.getElementsByClassName('close')[0];
+
+let jumble, currentQuestion
+const questionElement = document.getElementById('question')
+const answerButtons = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
-rulesButton.addEventListener('click', rulesGame)
+
+rulesButton.onclick = function() {
+    rulesContainer.style.display = 'block';
+}
+
+span.onclick = function(event) {
+    rulesContainer.style.display = 'none';
+}
 
 function startGame() {
     startButton.classList.add('hide')
+    rulesButton.classList.add('hide')
+    jumble = questions.sort(() => Math.random() - .5)
+    currentQuestion = 0
+    questionContainer.classList.remove('hide')
+    nextQuestion()
 
-}
-
-function rulesGame() {
-    ruleBox.removeProperty('display')
 }
 
 function nextQuestion() {
-
+    showQuestion(jumble[currentQuestion])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtons.appendChild(button)
+    })
+}
+
+function selectAnswer(e) {
     
 }
+
+const questions = [
+    {
+        question: 'What song are these lyrics from? "...Oh momma dear, were not the fortunate ones"' ,
+        answers: [
+            { text:'Girls Just Want to Have fun', correct: true},
+            { text:'Living on a Prayer', correct: false},
+            { text:"Don't stop believing", correct: false},
+            { text:'I want to know what love is', correct: false}
+        ]
+    }
+]
