b9c7e4231716f85b9f4735c00f79fb8ca73b91c6
Sharpryan20 <sharpryan01@icloud.com>
2021-10-29 16:52:24 +0000

    Added CSS stylings to rules-container

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,19 +89,22 @@ function setStatusClass(element, correct) {
     }
 }
 
-function increaseScore() {
-    
-    let currentScore = parseInt(document.getElementById('score').innerText)
-    document.getElementById('score').innerText = ++currentScore;
+function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
 const questions = [
     {
         question: 'What song are these lyrics from? "...Oh momma dear, were not the fortunate ones"' ,
-        choice1:'Girls Just Want to Have fun',
-        choice2:'Living on a Prayer',
-        choice3:"Don't stop believing",
-        choice4:'I want to know what love is',
+        answers: [
+            { text:'Girls Just Want to Have fun', correct: true},
+            { text:'Living on a Prayer', correct: false},
+            { text:"Don't stop believing", correct: false},
+            { text:'I want to know what love is', correct: false}
+        ]
     },
     {
         question: 'What song are these lyrics from? "...every now and then I get a little bit lonely"' ,
@@ -139,22 +142,94 @@ const questions = [
             { text:'Simple Minds', correct: false}
         ]
     },
+    {
+        question: 'What song are these lyrics from? "...Just beat it (beat it), beat it (beat it)"' ,
+        answers: [
+            { text:'Beat it', correct: true},
+            { text:'Sweet Child O Mine', correct: false},
+            { text:"Billie Jean", correct: false},
+            { text:'Jump', correct: false}
+        ]
+    },
+    {
+        question: 'What song are these lyrics from? "...All night long (all night), All night (all night)"' ,
+        answers: [
+            { text:'The Night Is Long', correct: false},
+            { text:"It's a Long Night", correct: false},
+            { text:'All Night Long', correct: true},
+            { text:'Night Long', correct: false}
+        ]
+    },
+    {
+        question: 'What song are these lyrics from? "...You were working as a waitress in a cocktail bar"' ,
+        answers: [
+            { text:'Another One Bites The Dust', correct: false},
+            { text:'Dont You Want Me', correct: true},
+            { text:"Betty Davies Eyes", correct: false},
+            { text:'Love Shack', correct: false}
+        ]
+    },
+    {
+        question: 'Who sung this song? "...Buying bread from a man in Brussels"' ,
+        answers: [
+            { text:'Men Without Hats', correct: false},
+            { text:'The Cure', correct: false},
+            { text:"U2", correct: false},
+            { text:'Men At Work', correct: true}
+        ]
+    },
+    {
+        question: 'Who sung this song? "...Oooh, he speaks the languages of love"' ,
+        answers: [
+            { text:'Blondie', correct: true},
+            { text:'Queen', correct: false},
+            { text:"Beastie Boys", correct: false},
+            { text:'Tommy Tutone', correct: false}
+        ]
+    },
+    {
+        question: 'What song are these lyrics from? "...When you wanna go to it (whats inside me?)"' ,
+        answers: [
+            { text:'Relax', correct: true},
+            { text:'The Safety Dance', correct: false},
+            { text:"You Give Love A Bad Name", correct: false},
+            { text:'Lets Groove', correct: false}
+        ]
+    },
+    {
+        question: 'What song are these lyrics from? "...The clock strikes upon the hour"' ,
+        answers: [
+            { text:'Time After Time', correct: false},
+            { text:"Girls Just Want To Have Fun", correct: false},
+            { text:'Dance With Somebody', correct: true},
+            { text:'Tainted Love', correct: false}
+        ]
+    },
+    {
+        question: 'What song are these lyrics from? "...You spin me right round, baby, right round"' ,
+        answers: [
+            { text:'All Night Long', correct: false},
+            { text:'You Spin Me Round', correct: true},
+            { text:"Should I Stay Or Should I Go", correct: false},
+            { text:'Everywhere', correct: false}
+        ]
+    },
+    {
+        question: 'Who sung this song? "...Welcome to the house of fun"' ,
+        answers: [
+            { text:'Karma Chameleon', correct: false},
+            { text:'Guns N Roses', correct: false},
+            { text:"Tears for Fears", correct: false},
+            { text:'Madness', correct: true}
+        ]
+    },
+    {
+        question: 'Who sung this song? "...With or without you"' ,
+        answers: [
+            { text:'U2', correct: true},
+            { text:'George Michael', correct: false},
+            { text:"Elton John", correct: false},
+            { text:'Fleetwood Mac', correct: false}
+        ]
+    },
 ]
-
-acceptingAnswers = true
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-        if(classToApply === 'correct') {
-            increaseScore()
-        }
-    })
-})
